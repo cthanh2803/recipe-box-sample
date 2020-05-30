f70ff74aeca91005886a1652191a6ee96ec60499
Thanh-pcu <thban2803@gmail.com>
2020-05-30 18:14:31 +0900

    Add recipes with ingradients and direction in nest form

---
+++ db/migrate/20200530065705_add_attachment_image_to_recipes.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class AddAttachmentImageToRecipes < ActiveRecord::Migration[6.0]
+  def self.up
+    change_table :recipes do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :recipes, :image
+  end
+end
